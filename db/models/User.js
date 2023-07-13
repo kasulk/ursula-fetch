f8d04f1db9cb11504f84e9f6c408f60e9ab18999
kasi <danielkaser@mailbox.org>
2023-07-13 18:04:51 +0200

    create models for logos and users

---
+++ db/models/User.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, trim: true, unique: true },
+  favoriteStocks: { type: [String] },
+});
+
+// check whether the model with this name has already been compiled and if yes, take the already compiled model
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
