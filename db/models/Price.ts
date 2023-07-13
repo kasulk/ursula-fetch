2630f1a08ccafa23f6bb55de9df0bf1afe8e6b84
kasi <danielkaser@mailbox.org>
2023-07-13 18:11:19 +0200

    create price/quote model--amend

--- db/models/Price.ts
+++ db/models/Price.ts
@@ -2,11 +2,13 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const quoteSchema = new Schema({
-  ticker: { type: mongoose.Schema.Types.ObjectId, ref: "Daten" },
-  quotes: Number,
-  //name: { type: String, required: true },
-});
+const quoteSchema = new Schema(
+  {
+    ticker: { type: mongoose.Schema.Types.ObjectId, ref: "Daten" },
+    quotes: Number,
+  },
+  { timestamps: true }
+);
 
 // check whether the model with this name has already been compiled and if yes, take the already compiled model
 const Quote = mongoose.models.Quote || mongoose.model("Quote", quoteSchema);
